0b6985d03625f2055a0f4dda6c398a82e7a79f1b
hbzhu <hbzhu@thoughtworks.com>
2020-03-20 17:14:09 +0800

    [feat] add transaction annotation.

--- src/test/java/com/poseiden/controller/HelloControllerTest.java
+++ src/test/java/com/poseiden/controller/HelloControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.poseiden.domain.role.Role.ADMIN;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles(profiles = "test")
+@Transactional
 public class HelloControllerTest {
 
     private MockMvc mockMvc;
